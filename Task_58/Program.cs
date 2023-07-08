37e9905ee7c741e7b71e61ac54344d31e1544721
Vladislav_Tishkovskij <vladislav.sergeevich.666@mail.ru>
2023-07-08 14:56:56 +0300

    Написали программу (Task_58)

---
+++ Task_58/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix1 = new int[,]
+        {
+            { 2, 4 },
+            { 3, 2 }
+        };
+
+        int[,] matrix2 = new int[,]
+        {
+            { 3, 4 },
+            { 3, 3 }
+        };
+
+        int[,] result = MultiplyMatrices(matrix1, matrix2);
+
+        Console.WriteLine("Result:");
+        PrintMatrix(result);
+    }
+
+    static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+    {
+        int rows1 = matrix1.GetLength(0);
+        int cols1 = matrix1.GetLength(1);
+        int cols2 = matrix2.GetLength(1);
+
+        int[,] result = new int[rows1, cols2];
+
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < cols2; j++)
+            {
+                for (int k = 0; k < cols1; k++)
+                {
+                    result[i, j] += matrix1[i, k] * matrix2[k, j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
